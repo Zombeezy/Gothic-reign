14483946633ebaf9e36880f3de4c9bb08a57e067
Zombeezy <Icanhazmailplz@yahoo.com>
2015-04-18 03:33:14 -0700

    Signed-off-by: Zombeezy <Icanhazmailplz@yahoo.com>

--- GothicDnd/main.cpp
+++ GothicDnd/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: Branden Hitt
  * Created on April 10, 2015, 4:10 PM
- * Last Modified: April 10, 2015, 4:10 PM
+ * Last Modified: April 18, 2015, 2:00 AM
  * 
  * 
  */
@@ -22,6 +22,7 @@ using namespace std;
 short d20Roll(short a);
 void loot();
 void buffs();
+void regurgitate(short r);
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Set the Random Number seed
@@ -34,19 +35,23 @@ int main(int argc, char** argv) {
     do{
     cout<<"Type 1: D20 Roll"<<endl;
     cout<<"Type 2: Enemy Buff Randomizer"<<endl;
-    cout<<"Type 3: "<<endl;
+    cout<<"Type 3: Character Flaw"<<endl;
     cout<<"Type 4: "<<endl;
     cout<<"Type 5: Wooden Chest"<<endl;
     cout<<"Type 6: Stone Chest"<<endl;
     cout<<"Type 7: Metal Chest"<<endl;
     cout<<"Type 8: Ornate Chest"<<endl;
-    cout<<"Type 9: "<<endl;
+    cout<<"Type 9: Regurgitate"<<endl;
     cout<<"Type T: Necronomicon"<<endl;
     //Read the choice
     char choice;
     cin>>choice;
     //Solve a problem that has been chosen.
     switch(choice){
+//****************************************************//
+//                    d20 Roll                        //
+//                                                    //
+//****************************************************//
             case '1':{
                 cout<<"***********************"<<endl;
                 short a, result;
@@ -58,6 +63,10 @@ int main(int argc, char** argv) {
                  cout<<"***********************"<<endl;
                 break;
             }
+//****************************************************//
+//                    Enemy Buffs                     //
+//                                                    //
+//****************************************************//
             case '2':{
                 cout<<"***********************"<<endl;
                 short e=0;
@@ -70,16 +79,98 @@ int main(int argc, char** argv) {
                 cout<<"***********************"<<endl;
                 break;
             }
+//****************************************************//
+//                    Character Flaws                 //
+//                                                    //
+//****************************************************//
             case '3':{
+                char reflaw;
+                char ready;
+                short flawNum;
                 cout<<"***********************"<<endl;
-                
+                cout<<"Ready for your character flaw?"<<endl;
+                cout<<"Type Y when ready:"<<endl;
+                cin>>ready;
+                do{
+                cout<<"***********************"<<endl;     
+                flawNum=rand()%25+1;
+                if (flawNum==1){
+                    cout<<"Giant feet: you have disadvantage when attempting to sneak"<<endl;
+                }else if (flawNum==2){
+                    cout<<"Vertigo: Scared of heights (-3 to initiation on high areas)"<<endl;
+                }else if (flawNum==3){
+                    cout<<"Indecisive: you have trouble making up your mind"<<endl;
+                }else if (flawNum==4){
+                    cout<<"Homesick: you always suggest the group go back"<<endl;
+                }else if (flawNum==5){
+                    cout<<"IBS: you have to announce to the group that you need to shit after every combat"<<endl;
+                }else if (flawNum==6){
+                    cout<<"Womanizer: you feel the need to use female characters selfishly (even allies)"<<endl;
+                }else if (flawNum==7){
+                    cout<<"Superstitious: you are terrified of black cats"<<endl;
+                }else if (flawNum==8){
+                    cout<<"Photophobia: scared of light"<<endl;
+                }else if (flawNum==9){
+                    cout<<"Illiterate: You can't read, not even your own dice rolls"<<endl;
+                }else if (flawNum==10){
+                    cout<<"Father Figure: you constantly try to make dad jokes"<<endl;
+                }else if (flawNum==11){
+                    cout<<"Philanthropist: when given the chance you will give away your money graciously"<<endl;
+                }else if (flawNum==12){
+                    cout<<"Antsy: you hate when your group stalls or takes time deciding things."<<endl;
+                    cout<<"To calm yourself, you repeatedly sing a nursery rhyme of your choice."<<endl;
+                }else if (flawNum==13){
+                    cout<<"Determined: you are obsessed with completing your tasks, even to your own detriment."<<endl;
+                }else if (flawNum==14){
+                    cout<<"Secretive: you always want to hide things from others."<<endl;
+                }else if (flawNum==15){
+                    cout<<"Ginger fever: whenever Kenneth lands a hit, be sure to compliment him loudly."<<endl;
+                }else if (flawNum==16){
+                    cout<<"Compulsive honesty: You cannot lie"<<endl;
+                }else if (flawNum==17){
+                    cout<<"Cruel: Your actions are motivated by cruelty. You contain no mercy"<<endl;
+                }else if (flawNum==18){
+                    cout<<"Disfigurement: Dude your face.... and your body.... what the fuck happened..."<<endl;
+                    cout<<"Change your name to Helga."<<endl;
+                }else if (flawNum==19){
+                    cout<<"Greedy: You don't share, ever."<<endl;
+                }else if (flawNum==20){
+                    cout<<"Hunted: You made an enemy in the past that now actively pursues you."<<endl;
+                }else if (flawNum==21){
+                    cout<<"Obese: You are really, really tubby. (-5 to speed)"<<endl;
+                }else if (flawNum==22){
+                    cout<<"Gambler: you can't resist a wager"<<endl;
+                }else if (flawNum==23){
+                    cout<<"Ozzy: terrified of flying monkeys & witches"<<endl;
+                }else if (flawNum==24){
+                    cout<<"Merciful: You never land a killing blow, and instead will only incapacitate your target"<<endl;
+                }else if (flawNum==25){
+                    cout<<"Orson Lannister: You are really, really dumb. You spend your free time "<<endl;
+                    cout<<" crushing beetles. CUH CUH!"<<endl;
+                }else{
+                    cout<<"Something fucked up in your program dude"<<endl;
+                }
+                cout<<"***********************"<<endl;
+                cout<<"Would you like to get another flaw? Type Y or N:"<<endl;
+                cin>>reflaw;
+                }while(reflaw=='y'||reflaw=='Y');
+                cout<<"***********************"<<endl;
+                cout<<"***********************"<<endl;
                 break;
             }
+//****************************************************//
+//                    Case 4                          //
+//                                                    //
+//****************************************************//
             case '4':{
                 cout<<"***********************"<<endl;
                 
                 break;
             }
+//****************************************************//
+//                    Wooden Chest                    //
+//                                                    //
+//****************************************************//
             case '5':{//Wooden Chest
                 cout<<"***********************"<<endl;
                 short playerNum, pick;
@@ -93,9 +184,13 @@ int main(int argc, char** argv) {
                 loot();
                 cout<<endl;
                 cout<<"***********************"<<endl;
-                 cout<<"***********************"<<endl;
+                cout<<"***********************"<<endl;
                 break;
             }
+//****************************************************//
+//                    Stone Chest                     //
+//                                                    //
+//****************************************************//
             case '6':{//Stone Chest
                 cout<<"***********************"<<endl;
                 short playerNum, pick;
@@ -113,9 +208,13 @@ int main(int argc, char** argv) {
                 loot();
                 cout<<endl;
                 cout<<"***********************"<<endl;
-                 cout<<"***********************"<<endl;
+                cout<<"***********************"<<endl;
                 break;
             }
+//****************************************************//
+//                    Metal Chest                     //
+//                                                    //
+//****************************************************//
             case '7':{//Metal Chest
                 cout<<"***********************"<<endl;
                 short playerNum, pick;
@@ -138,9 +237,13 @@ int main(int argc, char** argv) {
                 loot();
                 cout<<endl;
                 cout<<"***********************"<<endl;
-                 cout<<"***********************"<<endl;
+                cout<<"***********************"<<endl;
                 break;
             }
+//****************************************************//
+//                    Ornate Chest                    //
+//                                                    //
+//****************************************************//
             case '8':{//Ornate Chest
                 cout<<"***********************"<<endl;
                 short playerNum, pick;
@@ -168,14 +271,39 @@ int main(int argc, char** argv) {
                 loot();
                 cout<<endl;
                 cout<<"***********************"<<endl;
-                 cout<<"***********************"<<endl;
-                break;
-            }
-            case '9':{
                 cout<<"***********************"<<endl;
-               
                 break;
             }
+//****************************************************//
+//                    Regurgitate                     //
+//                                                    //
+//****************************************************//
+            case '9':{
+                char regCho;
+                short regurge;
+                cout<<"***********************"<<endl;
+                cout<<"Would you like to cast this on yourself, an ally, or an enemy:"<<endl;
+                cout<<"Type either (A,E,Y):"<<endl;
+                cin>>regCho;
+                cout<<"***********************"<<endl;
+                if (regCho=='a'||regCho=='A'){
+                    regurge=rand()%5+1;
+                }else if (regCho=='e'||regCho=='E'){
+                    regurge=rand()%10+1;
+                }else if (regCho=='y'||regCho=='Y'){
+                    regurge=rand()%15+1;
+                }else{
+                    cout<<"incorrect input"<<endl;
+                }
+                regurgitate(regurge);
+                cout<<"***********************"<<endl;
+                cout<<"***********************"<<endl;
+                break;
+            }
+//****************************************************//
+//                    Necronomicon                    //
+//                                                    //
+//****************************************************//
             case 't':
             case 'T':{
                 cout<<"***********************"<<endl;
@@ -347,7 +475,7 @@ void loot(){
         }else if(lootRoll==42){
             cout<<"Villager's Disguise"<<endl;
         }else if(lootRoll==43){
-            cout<<"A poisoned goblet"<<endl;
+            cout<<"A poison bead and goblet"<<endl;
         }else if(lootRoll==44){
             cout<<"Igor's Bell"<<endl;
         }else if(lootRoll==45){
@@ -400,5 +528,45 @@ void buffs(){
         cout<<"Enemy buff: Holy Water (Apparitions do half damage to this enemy)"<<endl;
     }else{
         cout<<"Enemy buff: No buff"<<endl;
+    }
+}
+//****************************************************//
+//                    Regurgitate                     //
+//                                                    //
+//****************************************************//
+void regurgitate(short r){
+    short num=r;
+    if(num==1){
+        cout<<"Red goo. Target gains 2 temporary health"<<endl;
+    }else if(num==2){
+        cout<<"Chewed up toilet seat. Target has now contracted syphilis"<<endl;
+    }else if(num==3){
+        cout<<"Rusty knife. Target loses 2 health"<<endl;
+    }else if(num==4){
+        cout<<"Crow bar. Target gains advantage on next dice roll"<<endl;
+    }else if(num==5){
+        cout<<"Jogging shoes. Target gains 15 speed for 8 hours"<<endl;
+    }else if(num==6){
+        cout<<"Fancy hat. Target gains 4 temporary health"<<endl;
+    }else if(num==7){
+        cout<<"Spiked club. Target loses 6 health."<<endl;
+    }else if(num==8){
+        cout<<"Old tire. Target falls to the ground and gets disadvantage on rolls."<<endl;
+    }else if(num==9){
+        cout<<"Puff of black smoke. Anyone within 15 feet radius is hidden."<<endl;
+    }else if(num==10){
+        cout<<"An old dog bone. Nothing happens."<<endl;
+    }else if(num==11){
+        cout<<"A baby bird. 'Squak squak squak!' "<<endl;
+    }else if(num==12){
+        cout<<"Black ink purges out and covers your eyes. Your next ability will miss."<<endl;
+    }else if(num==13){
+        cout<<"A black cat. It scampers away."<<endl;
+    }else if(num==14){
+        cout<<"You cough up a goblet with a bead of poison. Keep them in your inventory."<<endl;
+    }else if(num==15){
+        cout<<"Your own stomach. You lose 15 health."<<endl;
+    }else {
+        cout<<"Check for fuck up"<<endl;
     }
 }
